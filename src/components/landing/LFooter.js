f2a2217add83021c701ac33ee0c3e75f22705e74
Edwin3002 <vargas11742@gmail.com>
2022-04-27 14:47:43 -0500

    landing responsive

--- src/components/landing/LFooter.js
+++ src/components/landing/LFooter.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Card, ListGroup, Table } from 'react-bootstrap'
+import { Table } from 'react-bootstrap'
 import '../../style/landingPage.css'
 
 const LFooter = () => {
     return (
 
-        <div className='foot2 mt-5 pb-4' style={{ background: '#3B3054' }}>
+        <div className='foot2 ' style={{ background: '#3B3054' }}>
 
-            <h2 className='text-center'>Asistente prof</h2>
+            <h2 className='text-center text-light'>Asistente prof</h2>
             <img className='d-flex m-auto' style={{width: '5%'}} src='https://i.ibb.co/4fh24Gm/Whats-App-Image-2022-04-25-at-10-24-26-PM-removebg-preview.png' alt='logo'/>
-            <Table className='foot2  table-borderless w-75 mx-auto my-4 text-light' responsive="sm" style={{ background: '#3B3054' }}>
-                <tbody className='text-light'>
+            <Table className='foot2  table-borderless w-75 mx-auto my-0 text-light' responsive="sm" style={{ background: '#3B3054' }}>
+                <tbody className='text-light tableTd flex-wrap'>
                     <tr>
                         <th>Conócenos</th>
                         <th>Gana dinero con nosotros</th>
--- src/components/landing/LInfo.js
+++ src/components/landing/LInfo.js
@@ -4,28 +4,28 @@ import '../../style/landingPage.css'
 
 const LInfo = () => {
     return (
-        <div style={{ background: '#4B3F6B' }}>
-            <div class="d-flex justify-content-center">
-                <Container className='text-center text-light'>
+        <div className='py-5' style={{ background: '#4B3F6B' }}>
+            <div class="d-flex  justify-content-center" style={{ background: '#4B3F6B' }}>
+                <Container className='text-light mx-3 d-flex align-content-around   flex-wrap'>
                     <h1 className='text-light fw-bold'> En "Tu Asistente Prof"</h1>
                     <p className='text-light'>Encuentra la información para tu futuro Profesional.</p>
-                    <Button className='btnMas '>Ver  mas</Button>
+                    <br/>
+                        <Button className='btnMas mx-5'>Ver  mas</Button>
                 </Container>
                 <Container className='d-flex'>
-                    <img className='d-flex margin-auto w-75 ' src="https://res.cloudinary.com/lau2401/image/upload/v1651015886/4002302_1_si42va.png" />
+                    <img className='d-flex margin-auto w-100 ' src="https://res.cloudinary.com/lau2401/image/upload/v1651015886/4002302_1_si42va.png" alt=' logo' />
                 </Container>
             </div>
             <Container className="my-5">
-                <h2 className="text-light text-center py-2" style={{ background: '#7B7784' }}>
+                <h2 className="text-light text-center py-2" style={{ background: '#544b6b' }}>
                     Estamos aquí para ayudarte.
                 </h2>
                 <p className="text-light text-center my-5">
                     Podras encontar diferentes espacios para explorar y asi tomar una buena decision.
                 </p>
             </Container>
-
-            <div className='d-flex justify-content-between mx-2' style={{ }}>
-                <Card className='rounded' style={{ width: '16rem'}}>
+            <div className='d-flex justify-content-around mx-2 flex-wrap' style={{}}>
+                <Card className='card-1' style={{ width: '16rem' }}>
                     <Card.Img className='rounded-circle p-2 icon' style={{ width: '50px', background: '#3B3054' }} src="https://res.cloudinary.com/lau2401/image/upload/v1651019668/colegio_1_1_xqgzqd.png" />
                     <Card.Body>
                         <Card.Title className='fw-bold'>Encuentra un centro de formacion según tu perfil.</Card.Title>
@@ -34,8 +34,8 @@ const LInfo = () => {
                         </Card.Text>
                     </Card.Body>
                 </Card>
-                <Card className='mt-5' style={{ width: '16rem' }}>
-                        <Card.Img className='rounded-circle p-2 icon' style={{ width: '50px', background: '#3B3054' }} src="https://res.cloudinary.com/lau2401/image/upload/v1651019668/beca_2_1_1_sziwbd.png" />
+                <Card className='card-2' style={{ width: '16rem' }}>
+                    <Card.Img className='rounded-circle p-2 icon' style={{ width: '50px', background: '#3B3054' }} src="https://res.cloudinary.com/lau2401/image/upload/v1651019668/beca_2_1_1_sziwbd.png" />
                     <Card.Body>
                         <Card.Title className='fw-bold'>Enterate de Becas y ofertas academicas.</Card.Title>
                         <Card.Text>
@@ -44,8 +44,8 @@ const LInfo = () => {
 
                     </Card.Body>
                 </Card>
-                <Card style={{ width: '16rem' }}>
-                    <Card.Img className='rounded-circle p-2 icon' style={{ width: '50px', background: '#3B3054' }} src="https://res.cloudinary.com/lau2401/image/upload/v1651019668/beca_2_1_1_sziwbd.png" />
+                <Card className='card-3' style={{ width: '16rem' }}>
+                    <Card.Img className='rounded-circle p-2 icon' style={{ width: '50px', background: '#3B3054' }} src="https://i.ibb.co/mS39Kv3/auricular-1-1.png" />
                     <Card.Body>
                         <Card.Title className='fw-bold'>Acompañamiento para elegir bien tu vocación.</Card.Title>
                         <Card.Text>
@@ -55,6 +55,15 @@ const LInfo = () => {
                     </Card.Body>
                 </Card>
             </div>
+            <Card className='card-4 m-auto' style={{ width: '20rem' }}>
+                <Card.Body>
+                    <Card.Title className='fw-bold text-light'>Boost your links today</Card.Title>
+                    <Card.Text>
+                        <Button className='btnMas d-flex m-auto'>Realizar test</Button>
+                    </Card.Text>
+
+                </Card.Body>
+            </Card>
         </div>
     )
 }
