c797e02b7b789f617a7b522eabdeba0ecaf2b4a8
Edwin3002 <vargas11742@gmail.com>
2022-05-02 18:18:50 -0500

    diseño de categorias de carreras

--- src/components/Unis.js
+++ src/components/Unis.js
@@ -1,20 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect,  } from 'react'
 import { Card, Container } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { paintCareerAsync } from '../redux/actions/actionUniversity'
+import '../style/unis.css'
 
 export const Unis = () => {
 
     const dispatch = useDispatch()
     const { careeries } = useSelector(store => store.careeries)
-    const [all, setAll] = useState(careeries)
 
 
 
     
     useEffect(() => {
         dispatch(paintCareerAsync())
-        setAll(careeries)
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
     return (
@@ -28,8 +27,16 @@ export const Unis = () => {
                                 <Card.Img className='border border-3 rounded' style={{ width: '100%' }} variant="top" src="https://i.ibb.co/R46Z3hP/Logo-de-la-Universidad-El-Bosque-svg.png" />
                             </div>
                             <Card.Body>
-                                <Card.Title className='fs-3'>{carrera.universidad} <span careeries="bi bi-star-fill" style={{ color: '#6ee6e6', float: 'right' }}></span></Card.Title>
-                                <button className='rounded-pill py-1 px-4' style={{ background: '#94e7e7', border: ' 2px solid fff' }}>Ciencias Exactas y Agrarias </button>
+                                <Card.Title className='fs-3'>{carrera.universidad} <span className="bi bi-star-fill" style={{ color: '#6ee6e6', float: 'right' }}></span></Card.Title>
+                                {/* <button className='rounded-pill py-1 px-4' id={carrera.area} style={{  border: ' 2px solid fff' }}>{carrera.area}</button> */}
+                                <button className='rounded-pill py-1 px-4' id={carrera.area} style={{  border: ' 2px solid fff' }}>
+                                {carrera.area === 'c' ? 'Administrativas y Contables' :
+                                carrera.area === 'h' ? ' Humanísticas y Sociales ' :
+                                carrera.area === 'a' ? ' Artísticas' :
+                                carrera.area === 's' ? 'Medicina y Cs. de la Salud ' :
+                                carrera.area === 'i' ? 'Ingeniería y Computación' :
+                                carrera.area === 'd' ? 'Defensa y Seguridad' :
+                                'Ciencias Exactas y Agrarias'}</button>
                                 <Card.Text>
                                     <p><span className='fw-bold fs-5'>Titulo: </span> Licenciatura {carrera.carrera}</p>
                                     <p><span className='fw-bold fs-5'>Titulo de formacion ofertada: </span> {carrera.titulo} </p>
@@ -40,51 +47,7 @@ export const Unis = () => {
                         </Container>
                     ))
                 }
-                <Container className='d-flex my-3 bg-white' style={{ minWidth: '500px', maxWidth: '700px' }}>
-                    <div className='m-auto d-flex' style={{ width: '150px' }}>
-                        <Card.Img className='border border-3 rounded' style={{ width: '100%' }} variant="top" src="https://i.ibb.co/R46Z3hP/Logo-de-la-Universidad-El-Bosque-svg.png" />
-                    </div>
-                    <Card.Body>
-                        <Card.Title className='fs-3'>Universidad El Bosque <span careeries="bi bi-star-fill" style={{ color: '#6ee6e6', float: 'right' }}></span></Card.Title>
-                        <button className='rounded-pill py-1 px-4' style={{ background: '#94e7e7', border: ' 2px solid fff' }}>Ciencias Exactas y Agrarias </button>
-                        <Card.Text>
-                            <p><span className='fw-bold fs-5'>Titulo: </span> Licenciatura de Ciencias Sociales</p>
-                            <p><span className='fw-bold fs-5'>Titulo de formacion ofertada: </span> Profesional </p>
-                            <p><span className='fw-bold'>Duracion: </span> 10 semestres</p>
-                            <a href='https://www.unbosque.edu.co/' className='fw-bold' target="_blank" rel='noreferrer'>Ver carrera </a>
-                        </Card.Text>
-                    </Card.Body>
-                </Container>
-                <Container className=' d-flex my-3 bg-white' style={{ minWidth: '500px', maxWidth: '700px' }}>
-                    <div className='m-auto d-flex' style={{ maxWidth: '150px' }}>
-                        <Card.Img className='border border-3 rounded' style={{ width: '100%' }} variant="top" src="https://i.ibb.co/WxZdyQ2/Osv-Nas-C-400x400-removebg-preview.png" />
-                    </div>
-                    <Card.Body>
-                        <Card.Title className='fs-3'>Universidad de La sabana<span careeries="bi bi-star-fill" style={{ color: '#6ee6e6', float: 'right' }}></span></Card.Title>
-                        <button className='rounded-pill py-1 px-4' style={{ background: '#ff8080', border: ' 2px solid fff' }}>Ciencias Exactas y Agrarias </button>
-                        <Card.Text>
-                            <p><span className='fw-bold fs-5'>Titulo: </span> Administrativas y Contables </p>
-                            <p><span className='fw-bold fs-5'>Titulo de formacion ofertada: </span> Profesional </p>
-                            <p><span className='fw-bold'>Duracion: </span> 9 semestres</p>
-                            <a href='https://www.unisabana.edu.co/' className='fw-bold' target="_blank" rel='noreferrer'>Ver carrera </a>
-                        </Card.Text>
-                    </Card.Body>
-                </Container>
-                <Container className=' d-flex my-3 bg-white' style={{ minWidth: '500px', maxWidth: '700px' }}>
-                    <div className='m-auto d-flex' style={{ width: '150px' }}>
-                        <Card.Img className='border border-3 rounded' style={{ width: '100%' }} variant="top" src="https://i.ibb.co/WxZdyQ2/Osv-Nas-C-400x400-removebg-preview.png" />
-                    </div>
-                    <Card.Body>
-                        <Card.Title className='fs-3'>Universidad de La sabana<span careeries="bi bi-star-fill" style={{ color: '#6ee6e6', float: 'right' }}></span></Card.Title>
-                        <button className='rounded-pill py-1 px-4' style={{ background: '#ff8080', border: ' 2px solid fff' }}>Ciencias Exactas y Agrarias </button>
-                        <Card.Text>
-                            <p><span className='fw-bold fs-5'>Titulo: </span> Administrativas y Contables </p>
-                            <p><span className='fw-bold fs-5'>Titulo de formacion ofertada: </span> Profesional </p>
-                            <p><span className='fw-bold'>Duracion: </span> 9 semestres</p>
-                            <a href='https://www.unisabana.edu.co/' className='fw-bold' target="_blank" rel='noreferrer'>Ver carrera </a>
-                        </Card.Text>
-                    </Card.Body>
-                </Container>
+                
             </div>
         </div>
     )
