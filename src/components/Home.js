5537da9d976a2024d92048f1534da864a2778f50
Edwin3002 <vargas11742@gmail.com>
2022-05-03 18:19:15 -0500

    diseño de cartas de 2 resultados

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import {  BecasShow } from './BecasShow';
 import { Quiz } from './Quiz';
 import { LFooter } from './landing/LFooter';
 
@@ -9,7 +8,6 @@ const Home = () => {
     return (
         <div>
             <Quiz></Quiz>
-            {/* <BecasShow/> */}
             <LFooter/>
         </div>
     );
--- src/components/ResultQuiz/CardResult.js
+++ src/components/ResultQuiz/CardResult.js
@@ -1,64 +1,69 @@
 import React, { useEffect } from 'react';
-import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import { infoChaside } from '../../data/chaside';
 import {
-  filterCareersync,
-  paintCareerAsync,
+    filterCareersync,
+    paintCareerAsync,
 } from '../../redux/actions/actionUniversity';
+import '../../style/unis.css'
 
 const CardResult = ({ item, setResultMatch }) => {
-  const dispatch = useDispatch();
-  const handleUniversity = () => {
-    dispatch(filterCareersync(item));
-    setResultMatch(true);
-  };
-  console.log(item);
+    const dispatch = useDispatch();
+    const handleUniversity = () => {
+        dispatch(filterCareersync(item));
+        setResultMatch(true);
+    };
+    console.log(item);
 
-  useEffect(() => {
-    dispatch(paintCareerAsync());
-  }, []);
+    useEffect(() => {
+        dispatch(paintCareerAsync());
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-  return (
-    <>
-      {infoChaside.map((ite) =>
-        ite.id === item ? (
-          <Card style={{ width: '80%', margin: '0 auto' }}>
-            <Card.Img
-              variant='top'
-              src={ite.imagen}
-              style={{ height: '300px' }}
-            />
-            <Card.Body>
-              <Card.Title>{ite.title}</Card.Title>
-            </Card.Body>
-            <Card.Body>
-              <Card.Title>Intereses</Card.Title>
-              <Card.Text>{ite.intereses}</Card.Text>
-            </Card.Body>
-            <Card.Body>
-              <Card.Title>Aptitudes</Card.Title>
-              <Card.Text>{ite.aptitudes}</Card.Text>
-            </Card.Body>
-            <ListGroup className='list-group-flush'>
-              <ListGroupItem>Cras justo odio</ListGroupItem>
-              <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-              <ListGroupItem>Vestibulum at eros</ListGroupItem>
-            </ListGroup>
-            <Card.Body>
-              <Card.Link
-                onClick={() => {
-                  handleUniversity();
-                }}
-              >
-                Ver Universidades
-              </Card.Link>
-            </Card.Body>
-          </Card>
-        ) : null
-      )}
-    </>
-  );
+    return (
+        <div className='rounded mx-2' >
+            {infoChaside.map((ite) =>
+                ite.id === item ? (
+                    // <div className='bg-warning m-auto ' style={{ minWidth: '500px' }}>
+                    <div className='p-2 bg-light rounded' >
+
+                        <div className='m-auto p-2' style={{ maxWidth: '500px', border: '5px solid #2ACFCF', borderRadius: '20px' }}>
+
+                            <img
+                                className='w-100 rounded'
+                                variant='top'
+                                alt='q'
+                                src={
+                                    ite.id === 'c' ? "https://res.cloudinary.com/edwin3002/image/upload/v1651613763/u/c_rwrkmu.jpg" :
+                                        ite.id === 'h' ? "https://res.cloudinary.com/edwin3002/image/upload/v1651613751/u/h_ce1m2k.jpg" :
+                                            ite.id === 'a' ? "https://res.cloudinary.com/edwin3002/image/upload/v1651613869/u/a_zb7e01.jpg" :
+                                                ite.id === 's' ? "https://res.cloudinary.com/edwin3002/image/upload/v1651613762/u/s_indnvz.webp" :
+                                                    ite.id === 'i' ? "https://res.cloudinary.com/edwin3002/image/upload/v1651613827/u/i_lccsi4.jpg" :
+                                                        ite.id === 'd' ? "https://res.cloudinary.com/edwin3002/image/upload/v1651613853/u/d_g4ibn7.jpg" :
+                                                            ite.id === 'e' ? "https://res.cloudinary.com/edwin3002/image/upload/v1651613848/u/e_fety8d.jpg" :
+                                                                'https://thumbs.dreamstime.com/b/sin-foto-ni-icono-de-imagen-en-blanco-cargar-im%C3%A1genes-o-falta-marca-no-disponible-pr%C3%B3xima-se%C3%B1al-silueta-naturaleza-simple-marco-215973362.jpg'
+                                }
+                            />
+                        </div>
+                        <Card.Body>
+                            <Card.Title className='fs-3 text-center' >{ite.title}</Card.Title>
+                            <Card.Title className='text-center'>Intereses</Card.Title>
+                            <Card.Text id={ite.id} className='fw-bold text-light p-4 text-center border border-dark rounded'>{ite.intereses}</Card.Text>
+                            <Card.Title className='text-center'>Aptitudes</Card.Title>
+                            <Card.Text id={ite.id} className='fw-bold text-light py-4 text-center border border-dark rounded'>{ite.aptitudes}</Card.Text>
+                            <Button onClick={() => {handleUniversity()}}>
+                                Ver Universidades
+                            </Button>
+                        </Card.Body>
+                    </div>
+                ) : null
+            )}
+            <div>
+                <p className='text-light'>hola</p>
+            </div>
+        </div>
+    );
 };
 
 export default CardResult;
