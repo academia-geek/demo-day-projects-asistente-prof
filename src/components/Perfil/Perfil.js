6e41a16c46be369ceb07ef063a9f3454f3a8de04
Edwin3002 <vargas11742@gmail.com>
2022-05-10 17:53:28 -0500

    perfil pdf y result

--- src/components/Perfil/Perfil.js
+++ src/components/Perfil/Perfil.js
@@ -13,22 +13,23 @@ const Perfil = ({ userV }) => {
   const { displayName, email, photoURL } = userV;
   const [result, setResult] = useState();
   const [data, setData] = useState([]);
-  console.log(data);
-  console.log(userV);
-  console.log(result);
-  
+  const [c1, setC1] = useState();
+  const [c2, setC2] = useState();
+  console.log(c1)
+  console.log(c2)
   useEffect(() => {
     if (userV) {
-      const data =  JSON.parse(localStorage.getItem('user'))
-      setResult(data);
+      const dt = JSON.parse(localStorage.getItem('user'))
+      setResult(dt);
+      const dataUser = JSON.parse(localStorage.getItem('f'))
+      setC1 (infoChaside.filter(i => i.id === dataUser[0].id))
+      setC2 (infoChaside.filter(i => i.id === dataUser[1].id))
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  
-  let a = 'hola'
+
   function generatorPDF() {
     const doc = new jsPDF();
-    console.log(a)
     //cuadrado
     doc.setFillColor(75, 63, 107);
     doc.rect(0, 0, 210, 40, 'F');
@@ -51,7 +52,7 @@ const Perfil = ({ userV }) => {
     doc.setFontSize(16);
     doc.setTextColor(0, 0, 0);
     doc.text(
-      `Hola ${userV.displayName} estos son los resultados del test CHASIDE:`,
+      `Hola estudiante, estos son los resultados del test CHASIDE:`,
       20,
       60
     );
@@ -65,11 +66,11 @@ const Perfil = ({ userV }) => {
     doc.rect(30, 85, 60, 8, 'F');
     doc.setFontSize(12);
     doc.setTextColor(0, 0, 0);
-    doc.text(`${data[0].title}`, 32, 90);
+    doc.text(`${c1[0].title}`, 32, 90);
     // doc.text(`${data[0].title}`, 35, 90);
 
     //img cuadro
-    doc.addImage(`${data[0].imagen}`, 30, 105, 60, 60);
+    doc.addImage(`${c1[0].imagen}`, 30, 105, 60, 60);
     // doc.addImage(`${data[0].imagen}`, 30, 95, 60, 60);
 
     //cuadro y text intereses
@@ -84,7 +85,7 @@ const Perfil = ({ userV }) => {
 
     doc.setFontSize(10);
     doc.setTextColor(252, 252, 252);
-    doc.text(`${data[0].intereses}`, 105, 100, {
+    doc.text(`${c1[0].intereses}`, 105, 100, {
       align: 'justify',
       maxWidth: 70,
     });
@@ -100,7 +101,7 @@ const Perfil = ({ userV }) => {
 
     doc.setFontSize(10);
     doc.setTextColor(252, 252, 252);
-    doc.text(`${data[0].aptitudes}`, 105, 130, {
+    doc.text(`${c1[0].aptitudes}`, 105, 130, {
       align: 'justify',
       maxWidth: 70,
     });
@@ -115,7 +116,7 @@ const Perfil = ({ userV }) => {
 
     doc.setFontSize(10);
     doc.setTextColor(252, 252, 252);
-    doc.text(`${data[0].carrera}`, 105, 160, {
+    doc.text(`${c1[0].carrera}`, 105, 160, {
       align: 'justify',
       maxWidth: 70,
     });
@@ -129,13 +130,13 @@ const Perfil = ({ userV }) => {
     doc.rect(30, 195, 60, 8, 'F');
     doc.setFontSize(12);
     doc.setTextColor(0, 0, 0);
-    doc.text(`${data[1].title}`, 32, 200, {
+    doc.text(`${c2[0].title}`, 32, 200, {
       align: 'justify',
       maxWidth: 70,
     });
 
     //img cuadro 2
-    doc.addImage(`${data[1].imagen}`, 30, 215, 60, 60);
+    doc.addImage(`${c2[0].imagen}`, 30, 215, 60, 60);
 
     //cuadro y text intereses 2
     doc.setFontSize(14);
@@ -147,7 +148,7 @@ const Perfil = ({ userV }) => {
 
     doc.setFontSize(10);
     doc.setTextColor(252, 252, 252);
-    doc.text(`${data[1].intereses}`, 105, 210, {
+    doc.text(`${c2[0].intereses}`, 105, 210, {
       align: 'justify',
       maxWidth: 70,
     });
@@ -162,7 +163,7 @@ const Perfil = ({ userV }) => {
 
     doc.setFontSize(10);
     doc.setTextColor(252, 252, 252);
-    doc.text(`${data[1].aptitudes}`, 105, 240, {
+    doc.text(`${c2[0].aptitudes}`, 105, 240, {
       align: 'justify',
       maxWidth: 70,
     });
@@ -177,12 +178,12 @@ const Perfil = ({ userV }) => {
 
     doc.setFontSize(10);
     doc.setTextColor(252, 252, 252);
-    doc.text(`${data[1].carrera}`, 105, 272, {
+    doc.text(`${c2[0].carrera}`, 105, 272, {
       align: 'justify',
       maxWidth: 70,
     });
 
-    doc.save(`${displayName}-Resultados-Chaside.pdf`);
+    doc.save(`Resultados-Chaside.pdf`);
   }
 
   useEffect(() => {
@@ -269,7 +270,7 @@ const Perfil = ({ userV }) => {
                 onClick={() => deleteuser()}
               >
                 <Card.Text className='TextBold '>
-                  <i class='bi bi-x-square-fill '></i>
+                  <i className='bi bi-x-square-fill '></i>
                 </Card.Text>
                 <Card.Text className='TextMuted fs-6'>
                   Eliminar Cuenta
